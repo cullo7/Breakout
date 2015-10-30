0a4c84ce85bcead7fe713a83bf08de0b462be8ef
cullo7 <acullo1@binghamton.edu>
2015-10-29 21:07:44 -0400

    Update Breakout.java

--- Breakout.java
+++ Breakout.java
@@ -4,27 +4,19 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-
-
 public class Breakout extends JPanel implements Runnable{
 
-
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
-
-
-	public static void main(String args[])
-	{
-	JFrame window = new JFrame("Breakout");//create frame
-	Breakout content = new Breakout();
-	window.setContentPane(content);//assign instance of class to pane
-	window.setSize(1000,1000);//size of window
-	window.setLocation(0,0);
-	window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//end when window is exited
-	window.setVisible(true);
-	window.setResizable(false);
+	
+	public static void main(String args[]){
+		JFrame window = new JFrame("Breakout");//create frame
+		Breakout content = new Breakout();
+		window.setContentPane(content);//assign instance of class to pane
+		window.setSize(1000,1000);//size of window
+		window.setLocation(0,0);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//end when window is exited
+		window.setVisible(true);
+		window.setResizable(false);
 	}
 
 	public static final int RECT_HEIGHT = 20;//bar height
@@ -44,23 +36,18 @@ public class Breakout extends JPanel implements Runnable{
 
 
 	public Breakout(){//constructor
-
 		Thread t1 = new Thread(this);
 		t1.start();//thread for bar movement
 		setBackground(Color.BLACK);
 
-		ActionListener action =  new ActionListener() {
+		ActionListener action =  new ActionListener(){
 			public void actionPerformed(ActionEvent evt){//action performed when action is called
-				if(bar != null)
-				{
+				if(bar != null){
 					bar.updateForNewFrame();
 					ball.updateForNewFrame();
-					if(ball.ballX <= 850 && ball.ballX >= 150 && ball.ballY <= 375 && ball.ballY >= 80)
-					{
-						for(int i = 0;i < 70;i++)
-						{
-							if(exists[i])
-							{
+					if(ball.ballX <= 850 && ball.ballX >= 150 && ball.ballY <= 375 && ball.ballY >= 80){
+						for(int i = 0;i < 70;i++){
+							if(exists[i]){
 								blocks[i].updateForNewFrame();
 							}
 						}
@@ -73,66 +60,63 @@ public class Breakout extends JPanel implements Runnable{
 		timer = new Timer(30, action);//call action every 30 milliseconds
 
 		addMouseListener(new MouseAdapter() {
-		public void mousePressed(MouseEvent evt) {
-        requestFocus();
-            }
+			
+			public void mousePressed(MouseEvent evt) {
+        			requestFocus();
+        		}
 		});//listen for mouse clicks
 
 		addFocusListener(new FocusListener() {
-		public void focusGained(FocusEvent evt) {
-			timer.start();
-			repaint();
-		}
+			
+			public void focusGained(FocusEvent evt) {
+				timer.start();
+				repaint();
+			}
 
-		public void focusLost(FocusEvent evt) {
-			timer.stop();
-			repaint();
-		}
+			public void focusLost(FocusEvent evt) {
+				timer.stop();
+				repaint();
+			}
 		});//Listen for focus - if window is the main window on the computer
 
 		addKeyListener(new KeyAdapter(){
 
-		public void keyPressed(KeyEvent evt){
-			int key = evt.getKeyCode();
+			public void keyPressed(KeyEvent evt){
+				int key = evt.getKeyCode();
 
-			if(key == KeyEvent.VK_LEFT){//left key on keyboard
-				setXDirection(-2);
-			}
-			else if(key == KeyEvent.VK_RIGHT){//right key on keyboard bar moves 2 pixels every 5 miliseconds
-				setXDirection(2);
-			}
-			else if(key == KeyEvent.VK_SPACE && ball.balls > 0 && winner == false){//spacebar, ball only moves is game isn't won and there are still balls left
-				if(!ball.isMoving){
-					ball.isMoving = true;
-					ball.northWest = true;//ball moves northwest
+				if(key == KeyEvent.VK_LEFT){//left key on keyboard
+					setXDirection(-2);
+				}
+				else if(key == KeyEvent.VK_RIGHT){//right key on keyboard bar moves 2 pixels every 5 miliseconds
+					setXDirection(2);
+				}
+				else if(key == KeyEvent.VK_SPACE && ball.balls > 0 && winner == false){//spacebar, ball only moves is game isn't won and there are still balls left
+					if(!ball.isMoving){
+						ball.isMoving = true;
+						ball.northWest = true;//ball moves northwest
+					}
 				}
 			}
-		}
 		
-		public void keyReleased(KeyEvent evt){//when key is released bar stops moving
-			int key = evt.getKeyCode();
-			if(key == KeyEvent.VK_LEFT){
-				setXDirection(0);
+			public void keyReleased(KeyEvent evt){//when key is released bar stops moving
+				int key = evt.getKeyCode();
+				if(key == KeyEvent.VK_LEFT){
+					setXDirection(0);
+				}
+				if(key == KeyEvent.VK_RIGHT){
+					setXDirection(0);
+				}
 			}
-			if(key == KeyEvent.VK_RIGHT){
-				setXDirection(0);
-			}
-		}
-			
 		});//listen for keyboard
-		for(int k = 0;k < 70;k++)
-		{
+		for(int k = 0;k < 70;k++){
 			exists[k] = true;
 		}
 	} //end of constructor
 		
-	public void run()//runs when thread starts
-	{
+	public void run(){//runs when thread starts
 		try{
 			while(true){
-
 				move();
-
 				Thread.sleep(5);
 			}
 		}
@@ -141,58 +125,51 @@ public class Breakout extends JPanel implements Runnable{
 		}
 	}
 	
-	public void move()//runs every 5 milliseconds
-	{
-		if(bar!=null)//if  bar exists
-		{
+	public void move(){//runs every 5 milliseconds
+		if(bar!=null){//if  bar exists
 		bar.squareLeft+=xDirection;
 		}
 	}
 	
-	public void setXDirection(int xdir)
-	{
+	public void setXDirection(int xdir){
 		xDirection = xdir;
 	}
 
 	public void paintComponent(Graphics g){
-		
 		super.paintComponent(g);//import paintComponent of superclass
 		Graphics2D g2 = (Graphics2D)g;
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);//makes diagnols/curves cleaner
+        	g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);//makes diagnols/curves cleaner
 
-		if(bar == null)
-		{
-		width = getWidth();
-		height = getHeight();
-		bar = new Bar(this);
-		ball = new Ball(bar,this);
-		blocks= new Block[70];
-		for(int i = 0;i < 70;i++)
-		{
-			blocks[i] = new Block(ball, i, this);//instantiating each block
-		}
+		if(bar == null){
+			width = getWidth();
+			height = getHeight();
+			bar = new Bar(this);
+			ball = new Ball(bar,this);
+			blocks= new Block[70];
+			for(int i = 0;i < 70;i++){
+					blocks[i] = new Block(ball, i, this);//instantiating each block
+			}
 		}
 		//instantiates classes if just starting the program
 
-		if(hasFocus())
+		if(hasFocus()){
 			g.setColor(Color.CYAN);
-		else
+		}
+		else{
 			g.setColor(Color.GREEN);
-
+		}
 		g.setColor(Color.MAGENTA);
 		if(hasFocus()){
 			g.drawString("Arrow Keys Move Square",7,20);
 			g.drawString("K, R, G, B Change Color",7,40);
 		}
-		else
+		else{
 			g.drawString("Click to activate",7,20);
-
+		}
 		g.drawString("Balls: "+ball.balls,7,60);
 
-		if(ball.balls == 0)
-		{
-			for(int i = 0;i < 70;i++)
-			{
+		if(ball.balls == 0){
+			for(int i = 0;i < 70;i++){
 				exists[i] = false;
 			}
 			g.setColor(Color.RED);
@@ -201,8 +178,7 @@ public class Breakout extends JPanel implements Runnable{
 			g.setFont(new Font("TimesRoman", Font.PLAIN, 25));
 			g.drawString("Your score was "+score,100,400);
 		}
-		if(winner)
-		{
+		if(winner){
 			ball.isMoving=false;
 			g.setColor(Color.BLACK);
 			g.setFont(new Font("TimesRoman", Font.PLAIN, 150));
@@ -215,11 +191,9 @@ public class Breakout extends JPanel implements Runnable{
 
 		bar.draw(g);
 		ball.draw(g);
-		for(int i = 0;i < 70; i++)
-		{
-			if(exists[i])
-			{
-			blocks[i].draw(g, i);//if it exists draw it
+		for(int i = 0;i < 70; i++){
+			if(exists[i]){
+				blocks[i].draw(g, i);//if it exists draw it
 			}
 		}
 	}
