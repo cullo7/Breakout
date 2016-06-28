b96ec74c5ac703845e46ad5dd8cf93e2881b0dd1
cullo7 <culloaiden3@gmail.com>
2016-06-27 21:23:24 -0400

    fixed bug in Bar

--- Bar.java
+++ Bar.java
@@ -3,28 +3,28 @@ package breakout;
 import java.awt.*;
 
 public class Bar{
-	public Ball ball;
-	int squareLeft;
-	int squareTop;
-	boolean left = false;
-	boolean right = false;
-	private final Breakout breakout;
-		
-	public Bar(Breakout breakinst){
-		this.breakout = breakinst;
-		squareLeft = 120;
-		squareTop = 620;
-	}
-		
-	void updateForNewFrame() { 
-        	if(squareLeft > breakout.width-3-breakout.RECT_LENGTH){//if bar is off screen put it back on screen
-            		squareLeft = (breakout.width - 3 - breakout.RECT_LENGTH);}
-		else if(squareLeft < 3){
-			squareLeft = 3;
-        	}
-		void draw(Graphics g){//draws bar
-			g.setColor(Breakout.squareColor);
-			g.fillRoundRect(squareLeft, squareTop, breakout.RECT_LENGTH, breakout.RECT_HEIGHT,20,20);
-		}
-	}
+  public Ball ball;
+  int squareLeft;
+  int squareTop;
+  boolean left = false;
+  boolean right = false;
+  private final Breakout breakout;
+
+  public Bar(Breakout breakinst){
+    this.breakout = breakinst;
+    squareLeft = 120;
+    squareTop = 620;
+  }
+
+  void updateForNewFrame() { 
+    if(squareLeft > breakout.width-3-breakout.RECT_LENGTH){//if bar is off screen put it back on screen
+      squareLeft = (breakout.width - 3 - breakout.RECT_LENGTH);}
+    else if(squareLeft < 3){
+      squareLeft = 3;
+    }
+  }
+  void draw(Graphics g){//draws bar
+    g.setColor(Breakout.squareColor);
+    g.fillRoundRect(squareLeft, squareTop, breakout.RECT_LENGTH, breakout.RECT_HEIGHT,20,20);
+  }
 }
